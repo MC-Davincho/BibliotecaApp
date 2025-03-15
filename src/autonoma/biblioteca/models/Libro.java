561926c4ffaeb03da1a56d22973a35294a5661c7
salome grajales martinez <salomegm0412@gmail.com>
2025-03-14 20:58:27 -0500

    se agrego la clase libro

---
+++ src/autonoma/biblioteca/models/Libro.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.biblioteca.models;
+
+/**
+ *
+ * @author salom
+ */
+public class Libro  {
+    private long id;
+    private String titulo;
+    private Autor autor;
+
+    public Libro(long id, String titulo, Autor autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    
+}

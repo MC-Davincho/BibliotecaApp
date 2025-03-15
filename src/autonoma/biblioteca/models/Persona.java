e79858a454bd5e9d341fe096421e281e41a71aef
David <davidcortesgil9@gmail.com>
2025-03-14 20:11:56 -0500

    Se agrega la clase Persona

---
+++ src/autonoma/biblioteca/models/Persona.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.biblioteca.models;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Persona {
+     private String nombre; 
+     private String documentoIdentidad;
+     private String correoElectronico; 
+
+    public Persona(String nombre, String documentoIdentidad, String correoElectronico) {
+        this.nombre = nombre;
+        this.documentoIdentidad = documentoIdentidad;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
